bb0a64daf278fde8be1b099c15caa994257fa623
rapha4lx <rapha4.lx@gmail.com>
2025-02-17 16:58:45 -0300

    removed ls -d to get directory

--- remove_42_header.sh
+++ remove_42_header.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-folders=$(ls -d */);
+# folders=$(ls -d */);
 
-for folder in $folders; do
-    echo $folder;
-    files=$(find $folder -type f -name "*.c");
-    for file in $files; do
-        if head -n 1 "$file" | grep -q '^/\*'; then
-			sed -i '1,11d' "$file";
-			echo "	$file Header Cleaned";
-		else
-			echo "	$file Dont Have Header";
-		fi
-    done
-done
+# for folder in $folders; do
+#     echo $folder;
+#     files=$(find $folder -type f -name "*.c");
+#     for file in $files; do
+#         if head -n 1 "$file" | grep -q '^/\*'; then
+# 			sed -i '1,11d' "$file";
+# 			echo "	$file Header Cleaned";
+# 		else
+# 			echo "	$file Dont Have Header";
+# 		fi
+#     done
+# done
 
 files=$(find ./ -type f -name "*.c");
 for file in $files; do
